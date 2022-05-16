3bf3ac9623f80bb808b0cfdf11a53a6ec5c60bf4
T-Excellence <86628988+T-Excellence@users.noreply.github.com>
2022-05-16 17:09:51 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var http=require("http");
+
+function callback(){
+    console.log("someone connects");
+}
+
+var server= http.createServer(function(request, response){
+callback();
+response.writeHead(200,{"Content-Type":"html"});
+response.end("<h1>Hello buddy</h1><br> welcome to my website.");
+
+}
+
+);
+server.listen(8081, "127.0.0.1");
+console.log(`server is listening on ${server.address()}`);
